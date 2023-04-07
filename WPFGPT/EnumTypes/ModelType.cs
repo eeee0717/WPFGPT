74d657f38d8dbcb1446d626e747eff64b1ec5102
eeee0717 <761050293@qq.com>
2023-04-07 17:10:59 +0800

    对话模型

--- WPFGPT/EnumTypes/ModelType.cs
+++ WPFGPT/EnumTypes/ModelType.cs
@@ -9,4 +9,8 @@ public enum ModelType
 {
     [Description("gpt-3.5-turbo")]
     Type = 0,
+    [Description("gpt-4")]
+    Type1 = 1,
+    [Description("gpt-4-32k")]
+    Type2 = 2,
 }
--- WPFGPT/ViewModels/MainWindowViewModel.cs
+++ WPFGPT/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,6 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Runtime.CompilerServices;
-
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,23 +17,21 @@ using WPFGPT.Models;
 
 namespace WPFGPT.ViewModels;
 
-public partial class MainWindowViewModel: ObservableObject
+public partial class MainWindowViewModel : ObservableObject
 {
     public ObservableCollection<ChatMessage> ChatObservableCollection { set; get; } = new();
     private ChatGpt _chatGpt;
-    
-    [ObservableProperty]
-    [NotifyCanExecuteChangedFor(nameof(ClickCommand))]
+
+    [ObservableProperty] [NotifyCanExecuteChangedFor(nameof(ClickCommand))]
     private string? _messageInput;
-    [ObservableProperty]
-    private object? _apiKey;
+
+    [ObservableProperty] private object? _apiKey;
     private Config? _config;
-    [ObservableProperty]
-    private int _systemType;
-    [ObservableProperty]
-    private int _modelType;
-    [ObservableProperty]
-    private int _maxToken;
+    [ObservableProperty] private int _systemType;
+    [ObservableProperty] private int _modelType;
+
+    [ObservableProperty] private int _maxToken = 100;
+
     public MainWindowViewModel()
     {
         this.Initialize();
@@ -52,6 +49,7 @@ public partial class MainWindowViewModel: ObservableObject
         {
             File.Create(configJson).Close();
         }
+
         string jsonString;
         using (StreamReader reader = new StreamReader(configJson))
         {
@@ -62,25 +60,30 @@ public partial class MainWindowViewModel: ObservableObject
         {
             this._config = JsonConvert.DeserializeObject<Config>(jsonString);
             this.ApiKey = this._config!.Api;
+            this._chatGpt = new ChatGpt(this.ApiKey!.ToString()!, this.MaxToken);
         }
         // else
         // {
         //     MessageBox.Show("Please Set The Api Key!", "Warning",MessageBoxButton.OK, MessageBoxImage.Warning);
         // }
     }
+
     [RelayCommand(CanExecute = nameof(CanClick))]
     private async Task Click()
     {
-        this._chatGpt = new ChatGpt(this.ApiKey!.ToString()!, this.MaxToken);
+        this._chatGpt.MaxToken = this.MaxToken;
         var chatMessage = new ChatMessage { IsSend = true, Message = MessageInput };
         this.ChatObservableCollection.Add(chatMessage);
         this.MessageInput = "";
 
-        await this._chatGpt.Chat(this.ChatObservableCollection, chatMessage.Message, this.SystemType);
+        await this._chatGpt.Chat(this.ChatObservableCollection, chatMessage.Message, this.SystemType, this.ModelType);
     }
+
     bool CanClick() => !string.IsNullOrEmpty(MessageInput);
 
-  
-
-   
+    [RelayCommand]
+    private void ClearClick()
+    {
+        this.ChatObservableCollection.Clear();
+    }
 }
